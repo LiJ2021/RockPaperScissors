b14c2bb51c25e8c6c31d3f7827f6855576afc17a
XCA Student <xcastudent@Admins-MacBook-Pro.local>
2022-05-02 14:15:57 -0400

    alert message changes

--- scriptRPS.js
+++ scriptRPS.js
@@ -2,7 +2,7 @@
 
 // The design of your webpage is completely up to you. The only requirements are as follows:
 // - A user is able to choose between three options, Rock, Paper, and Scissors.
-// - When an option is chosen, it must be obvious to the use which option has been chosen.
+// - When an option is chosen, it must be obvious to the user which option has been chosen.
 // - When clicking a 'submit' button, the user's choice will be submitted and it will be compared against a randomly generated computer choice.
 // - The user is then presented with an alert that tells them if they won, tied, or lost, and includes the user's input as well as the computer's input.
 // alert("Hello Player1!")
@@ -26,30 +26,30 @@ const computersAnswer = Math.floor(Math.random() * 3)
 
     if (userSelection === 0 && computersAnswer === 0) {
        
-        alert("Tie! try again")
+        alert("You chose Rock: Tie! try again")
         } 
      else if (userSelection === 0 && computersAnswer === 1) {
         
-        alert("Paper Covers Rock! Computer Wins!")
+        alert("You chose Rock: Paper Covers Rock! Computer Wins!")
         }
     else if (userSelection === 0 && computersAnswer === 2) {
        
-        alert("Rock Crushes Scissors! Player Wins!")
+        alert("You chose Rock: Rock Crushes Scissors! Player Wins!")
         }
     
 
 //for paper 
         else if (userSelection === 1 && computersAnswer === 0) {
     
-     alert("Paper Covers Rock! Player Wins!")
+     alert("You chose Paper: Paper Covers Rock! Player Wins!")
         } 
         else if (userSelection === 1 && computersAnswer === 1) {
         
-            alert("Tie! try again")
+            alert("You chose Paper: Tie! try again")
         }
         else if (userSelection === 1 && computersAnswer === 2) {
             
-            alert("Scissors cut Paper! Computer Wins!")
+            alert("You chose Paper: Scissors cut Paper! Computer Wins!")
         }
     
     
@@ -58,15 +58,15 @@ const computersAnswer = Math.floor(Math.random() * 3)
 
     else if (userSelection === 2 && computersAnswer === 0) {
    
-    alert("Rock crushes Scissors! Computer Wins!")
+    alert("You chose Scissors: Rock crushes Scissors! Computer Wins!")
     } 
     else if (userSelection === 2 && computersAnswer === 1) {
        
-        alert("Scissors cut Paper! Player Wins!")
+        alert("You chose Scissors: Scissors cut Paper! Player Wins!")
     }
     else if (userSelection === 2 && computersAnswer === 2) {
       
-        alert("Tie! try again")
+        alert("You chose Scissors: Tie! try again")
     }
 
 
